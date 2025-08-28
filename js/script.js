59ae2d7bd4b524ed68b0824f5f27b8bf710bb1d8
Sankajith D. Jinasena <22cds0431@ms.sab.ac.lk>
2025-08-28 11:10:52 +0530

    Updated

--- js/script.js
+++ js/script.js
@@ -1,54 +1,57 @@
-/* Toggle Icon navbar */
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// Typed JS initialization
+const typed = new Typed('.multiple-text', {
+    strings: ['Gen AI Enthusiast', 'Web Developer', 'ML Enthusiast'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+// Toggle menu
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+// Scroll sections active link
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset+height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector("header nav a[href*=' + id + ']").classList(addEventListener.add('active'));
-            })
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
         }
-
     });
+
+    // Sticky navbar
+    const header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar when click navbar link (scroll)
+    navbar.classList.remove('active');
 };
 
-let header = document.querySelector('header');
-
-header.classList.toggle('sticky', window.scrollY > 100)
-
-/* Remove toggle icon and navbar when click navbar links*/
-
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-/* Scroll Reveal */
-
+// Scroll Reveal
 ScrollReveal({
     reset: true,
-distance :'80px',
-duration : 2000,
-delay : 200
+    distance: '80px',
+    duration: 2000,
+    delay: 200
 });
 
-ScrollReveal().reveal('.home-content', { origin: 'top' });
-
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'left' });
-
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
